44056bf9884020aef93aed89e844d5ef8dd4a546
ThomasFr45 <thomasfran1935@gmail.com>
2022-04-16 16:52:42 +0200

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(window.pageYOffset);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React or not
-        </a>
-      </header>
+    <div className="home-container">
+      <img src="https://i.imgur.com/gjVmRDs.jpg" alt="" className="home-image" />
+      <p className="home-text">
+        Hi, welcome, you can find here some informations about racial crafting !
+      </p>
+      <p className="home-text">
+        I hope this website can be usefull to some, if you encounter any problem
+        here or in game, feel free to send us a message using the contact page,
+        we would be happy to be of help.
+      </p>
+      <p className="home-text">If by any chance you are not interested in crafting it by yourself, you can go to the market section and directly order whatever you need. (Note that we may not have all the craftable items for now, but fret not ! We are currently working on it !).</p>
+      <p className="home-text">Before you go ordering, note that orders can take some time to prepare, as our stocks are limited, and multiple clients may order at the same time.</p>
+      <p className="home-text-last"><b>Thanks for reading, enjoy your time here !</b></p>
     </div>
   );
 }
