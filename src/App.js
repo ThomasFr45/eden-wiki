cdae8ed2a86a5a1d82c14d7df5b2bf573fbdbcd4
ThomasFr45 <thomasfran1935@gmail.com>
2022-04-16 17:11:15 +0200

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
-  console.log(window.pageYOffset);
   return (
-    <div className="home-container">
-      <img src="https://i.imgur.com/gjVmRDs.jpg" alt="" className="home-image" />
-      <p className="home-text">
-        Hi, welcome, you can find here some informations about racial crafting !
-      </p>
-      <p className="home-text">
-        I hope this website can be usefull to some, if you encounter any problem
-        here or in game, feel free to send us a message using the contact page,
-        we would be happy to be of help.
-      </p>
-      <p className="home-text">If by any chance you are not interested in crafting it by yourself, you can go to the market section and directly order whatever you need. (Note that we may not have all the craftable items for now, but fret not ! We are currently working on it !).</p>
-      <p className="home-text">Before you go ordering, note that orders can take some time to prepare, as our stocks are limited, and multiple clients may order at the same time.</p>
-      <p className="home-text-last"><b>Thanks for reading, enjoy your time here !</b></p>
-    </div>
+    <Home />
   );
 }
 
